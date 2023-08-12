4f5ad23a3daf57ba6a325b94de678643ee15b5c6
ruxia-TJY <ruxia.tjy@qq.com>
2023-08-12 14:51:47 +0800

    添加测试文件

---
+++ test.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<string.h>
+#include "base64.h"
+
+int main(int argc,char *argv[])
+{
+    if(argc == 1){
+        printf("-e encode String\n");
+        printf("-d decode String");
+        return 0;
+    }
+    
+    if(!strcmp(argv[1],"-e")){
+        printf("%s",base64EncodeStr(argv[2]));  
+    }
+    else if(!strcmp(argv[1],"-d")){
+        printf("%s",base64DecodeStr(argv[2]));
+    }
+    
+}
